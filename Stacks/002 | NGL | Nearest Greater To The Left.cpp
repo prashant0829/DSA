d37f1451442f5b457571da8adf36367840f97772
Prashant Bansal <prashantbansal500@gmail.com>
2026-03-29 10:12:04 +0530

    NGL Added

---
+++ Stacks/002 | NGL | Nearest Greater To The Left.cpp
@@ -0,0 +1,45 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+
+    cout << "Enter the size of the array\n";
+    cin >> n;
+    vector<int> v(n);
+    cout << "Your array size is " << n << "\n";
+    cout << "Enter the array\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    stack<int> s;
+    vector<int> r(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        while (!s.empty() && s.top() <= v[i])
+        {
+            s.pop();
+        }
+
+        r[i] = s.empty() ? -1 : s.top();
+
+        s.push(v[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << r[i] << " ";
+    }
+
+    return 0;
+}
