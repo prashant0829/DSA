4feaad7a27701d84218b3f00a82be2a1320baf3f
Prashant Bansal <prashantbansal500@gmail.com>
2026-05-12 23:36:28 +0530

    Added permutations with case

---
+++ Recursion/012 | Permutations with case.cpp
@@ -0,0 +1,51 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <cmath>
+#include <set>
+
+using namespace std;
+
+string toggleCase(char c)
+{
+    if (islower(c))
+    {
+        c = toupper(c);
+    }
+    else if (isupper(c))
+    {
+        c = tolower(c);
+    }
+    return string(1, c);
+}
+
+void solve(string input, string output)
+{
+    if (input.length() == 0)
+    {
+        cout << output << " \n";
+        return;
+    }
+
+    char ch = input[0];
+    input.erase(input.begin());
+
+    string output1 = output + toggleCase(ch);
+    string output2 = output + ch;
+
+    solve(input, output1);
+    solve(input, output2);
+}
+int main()
+{
+    // Write C++ code here
+    string s;
+    cout << "Enter a string\n";
+    cin >> s;
+    cout << "Permutations with spaces are as follows\n";
+    string output = "";
+    output += s[0];
+    s.erase(s.begin());
+    solve(s, output);
+
+    return 0;
+}
