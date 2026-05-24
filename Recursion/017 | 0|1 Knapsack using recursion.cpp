41eb6c6a3130e4b132df3789ffae5643b4dfd11c
Prashant Bansal <prashantbansal500@gmail.com>
2026-05-24 22:54:34 +0530

    Added 0|1 knapsack using recursion

---
+++ Recursion/017 | 0|1 Knapsack using recursion.cpp
@@ -0,0 +1,56 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int knapsack(vector<int> &v, vector<int> &w, int k, int n)
+{
+    if (n == 0 || k == 0)
+    {
+        return 0;
+    }
+
+    if (w[n - 1] <= k)
+    {
+        return max(v[n - 1] + knapsack(v, w, k - w[n - 1], n - 1), knapsack(v, w, k, n - 1));
+    }
+    else
+    {
+        return knapsack(v, w, k, n - 1);
+    }
+}
+
+int main()
+{
+    int k;
+    cout << "Enter the size of the knapsack\n";
+    cin >> k;
+
+    int n;
+    cout << "Enter the number of available items\n";
+    cin >> n;
+
+    vector<int> v(n);
+    cout << "Enter the value of the items\n";
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        v[i] = a;
+    }
+
+    vector<int> w(n);
+    cout << "Enter the weights of the items\n";
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        w[i] = a;
+    }
+
+    cout << "Maximum value: " << knapsack(v, w, k, n);
+
+    return 0;
+}
